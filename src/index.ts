c034c44ff6a2d849082d54ae5ae9b2e68105ddb1
NicoToff <toffolo01@hotmail.com>
2022-05-11 18:54:46 +0200

    Set up of first ERD in prisma

--- src/index.ts
+++ src/index.ts
@@ -3,37 +3,28 @@
 const btnShow = document.getElementById("display") as HTMLButtonElement;
 const tableBody = document.getElementById("table-body") as HTMLTableSectionElement;
 
-/* Type declaration to match an element from the database */
-type Connection = {
-    id: string;
-    hostname: string;
-    protocol: string;
-    method: string;
-    date: Date;
-};
+// btnShow.addEventListener("click", () => {
+//     btnShow.setAttribute("disabled", "true");
+//     const toServer = new XMLHttpRequest();
+//     toServer.open("POST", "/api/show", true);
+//     toServer.addEventListener("load", () => {
+//         displayTable(tableBody, JSON.parse(toServer.response).connections);
+//     });
+//     toServer.send();
+// });
 
-btnShow.addEventListener("click", () => {
-    btnShow.setAttribute("disabled", "true");
-    const toServer = new XMLHttpRequest();
-    toServer.open("POST", "/api/show", true);
-    toServer.addEventListener("load", () => {
-        displayTable(tableBody, JSON.parse(toServer.response).connections);
-    });
-    toServer.send();
-});
-
-function displayTable(tableBody: HTMLTableSectionElement, response: Connection[]): void {
-    response.forEach(connection => {
-        const tr: HTMLTableRowElement = document.createElement("tr");
-        const arr_data: string[] = [connection.hostname, connection.protocol, connection.method];
-        arr_data.forEach(elem => {
-            const td: HTMLTableCellElement = document.createElement("td");
-            td.textContent = elem;
-            tr.append(td);
-        });
-        const tdDate: HTMLTableCellElement = document.createElement("td");
-        tdDate.textContent = new Date(connection.date).toISOString();
-        tr.append(tdDate);
-        tableBody.append(tr);
-    });
-}
+// function displayTable(tableBody: HTMLTableSectionElement, response: Connection[]): void {
+//     response.forEach(connection => {
+//         const tr: HTMLTableRowElement = document.createElement("tr");
+//         const arr_data: string[] = [connection.hostname, connection.protocol, connection.method];
+//         arr_data.forEach(elem => {
+//             const td: HTMLTableCellElement = document.createElement("td");
+//             td.textContent = elem;
+//             tr.append(td);
+//         });
+//         const tdDate: HTMLTableCellElement = document.createElement("td");
+//         tdDate.textContent = new Date(connection.date).toISOString();
+//         tr.append(tdDate);
+//         tableBody.append(tr);
+//     });
+// }
